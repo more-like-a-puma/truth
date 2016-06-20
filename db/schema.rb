3eed4a71e4a80909da2518c200b78151433d116f
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-20 18:20:15 +1000

    adding new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620010327) do
+ActiveRecord::Schema.define(version: 20160620075110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20160620010327) do
     t.text     "content"
     t.text     "image"
     t.text     "typing"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "seen",       default: false
   end
 
   create_table "users", force: :cascade do |t|
