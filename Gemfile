88ec8ec55fc7627b4c8375fa1961665731634b2f
more-like-a-puma <jessicamaycreative@gmail.com>
2016-06-21 15:54:28 +1000

    aaaaarrrrgh

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'thin'
+
 gem 'websocket-rails'
 
 gem 'faye-websocket', '0.10.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620075110) do
+ActiveRecord::Schema.define(version: 20160621054053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160620075110) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.boolean  "seen",       default: false
+    t.integer  "target"
   end
 
   create_table "users", force: :cascade do |t|
